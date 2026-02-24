1a5e95371cc303cabbab6fd02cb123d2afa4fbc8
LuisMantilla28 <113846460+LuisMantilla28@users.noreply.github.com>
2026-02-24 11:55:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,6 @@ st.set_page_config(
     layout="wide",
 )
 
-hide_streamlit_style = """
-<style>
-[data-testid="stSidebarNav"] {display: none;}
-header {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 navigation.show()
