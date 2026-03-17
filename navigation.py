2883fce2f74e36e1858e983fb4219b477bac1364
LuisMantilla28 <113846460+LuisMantilla28@users.noreply.github.com>
2026-03-17 18:30:21 -0500

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -23,8 +23,7 @@ def show():
             st.switch_page("pages/SQL.py")
 
         if st.button("📈 Series de tiempo"):
-            st.switch_page("pages/Series_tiempo/multivariado/multivaridado.py
-")
+            st.switch_page("pages/Series_tiempo/multivariado/multivaridado.py")
 
         if st.button("💰 Modelado financiero"):
             st.write("Próximamente")
