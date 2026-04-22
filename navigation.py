f06fbdc3e5041c58b66b7d8cfa4141202a9f594e
Luis Mantilla <113846460+LuisMantilla28@users.noreply.github.com>
2026-04-22 10:51:24 -0500

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -1,4 +1,58 @@
 import streamlit as st
 
 
-def show(): custom_style = """ <style> [data-testid="stSidebarNav"] { display: none; } header { visibility: hidden; } [data-testid="stSidebar"] { min-width: 18rem !important; max-width: 18rem !important; transform: none !important; visibility: visible !important; } [data-testid="stSidebar"][aria-expanded="false"] { min-width: 18rem !important; max-width: 18rem !important; } [data-testid="stSidebar"][aria-expanded="false"] > div:first-child { margin-left: 0rem !important; } </style> """ st.markdown(custom_style, unsafe_allow_html=True) with st.sidebar: st.title("📌 Navegación") st.markdown("---") st.markdown("### Secciones") if st.button("🏠 Inicio"): st.switch_page("app.py") if st.button("🟦 SQL"): st.switch_page("pages/SQL.py") if st.button("📈 Series de tiempo"): st.switch_page("pages/series_tiempo.py") if st.button("💰 Modelado financiero"): st.switch_page("pages/modelado_financiero.py") if st.button("🤖 Machine Learning"): st.write("Próximamente") st.markdown("---") st.markdown("### Enlaces") st.link_button("GitHub", "https://github.com/LuisMantilla28/portfolio_streamlit") st.link_button("LinkedIn", "https://www.linkedin.com/in/luis-enrique-mantilla-sanabria-905a01271/")
+def show():
+    custom_style = """
+    <style>
+    [data-testid="stSidebarNav"] {
+        display: none;
+    }
+
+    header {
+        visibility: hidden;
+    }
+
+    [data-testid="stSidebar"] {
+        min-width: 18rem !important;
+        max-width: 18rem !important;
+        transform: none !important;
+        visibility: visible !important;
+    }
+
+    [data-testid="stSidebar"][aria-expanded="false"] {
+        min-width: 18rem !important;
+        max-width: 18rem !important;
+    }
+
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        margin-left: 0rem !important;
+    }
+    </style>
+    """
+    st.markdown(custom_style, unsafe_allow_html=True)
+
+    with st.sidebar:
+        st.title("📌 Navegación")
+
+        st.markdown("---")
+        st.markdown("### Secciones")
+
+        if st.button("🏠 Inicio"):
+            st.switch_page("app.py")
+
+        if st.button("🟦 SQL"):
+            st.switch_page("pages/SQL.py")
+
+        if st.button("📈 Series de tiempo"):
+            st.switch_page("pages/series_tiempo.py")
+
+        if st.button("💰 Modelado financiero"):
+            st.switch_page("pages/modelado_financiero.py")
+            
+        if st.button("🤖 Machine Learning"):
+            st.write("Próximamente")
+
+        st.markdown("---")
+        st.markdown("### Enlaces")
+        st.link_button("GitHub", "https://github.com/LuisMantilla28/portfolio_streamlit")
+        st.link_button("LinkedIn", "https://www.linkedin.com/in/luis-enrique-mantilla-sanabria-905a01271/")
